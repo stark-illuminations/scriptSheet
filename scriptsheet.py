8784e47998edcccb287a4ae8605c73c8f5b56d16
stark-illuminations <starkwinter129@gmail.com>
2023-12-10 11:34:26 -0600

    Update scriptsheet.py

--- scriptsheet.py
+++ scriptsheet.py
@@ -8,24 +8,51 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument("source", help="The source file. A .md file.")
 parser.add_argument("dest", help="The dest file. A .md file.")
-parser.add_argument("--q", help="The prefix for cues in comments. \
-    Defaults to 'Q'.")
-parser.add_argument("--i", help="The prefix for times in comments \
-    Defaults to 'I'.")
-parser.add_argument("--l", help="The prefix for labels in comments \
-    Defaults to 'Label:'.")
-parser.add_argument("--n", help="The prefix for labels in comments \
-    Defaults to 'Notes:'.")
-parser.add_argument("--s", help="The prefix for scenes in comments \
-    Defaults to 'Scene:'.")
-parser.add_argument("--m", help="The prefix for marks in comments \
-    Defaults to 'M'.")
-parser.add_argument("--b", help="The prefix for blocks in comments \
-    Defaults to 'B'.")
-parser.add_argument("--f", help="The prefix for follows in comments \
-    Defaults to 'F'.")
-parser.add_argument("--x", help="The prefix for executes in comments \
-    Defaults to 'X'.")
+parser.add_argument(
+    "--q",
+    help="The prefix for cues in comments. \
+    Defaults to 'Q'.",
+)
+parser.add_argument(
+    "--i",
+    help="The prefix for times in comments \
+    Defaults to 'I'.",
+)
+parser.add_argument(
+    "--l",
+    help="The prefix for labels in comments \
+    Defaults to 'Label:'.",
+)
+parser.add_argument(
+    "--n",
+    help="The prefix for labels in comments \
+    Defaults to 'Notes:'.",
+)
+parser.add_argument(
+    "--s",
+    help="The prefix for scenes in comments \
+    Defaults to 'Scene:'.",
+)
+parser.add_argument(
+    "--m",
+    help="The prefix for marks in comments \
+    Defaults to 'M'.",
+)
+parser.add_argument(
+    "--b",
+    help="The prefix for blocks in comments \
+    Defaults to 'B'.",
+)
+parser.add_argument(
+    "--f",
+    help="The prefix for follows in comments \
+    Defaults to 'F'.",
+)
+parser.add_argument(
+    "--x",
+    help="The prefix for executes in comments \
+    Defaults to 'X'.",
+)
 
 
 args = parser.parse_args()
@@ -60,13 +87,22 @@ def split_annotations(lines):
 
 
 def process_cues(
-        annotations, cue_prefix, time_prefix, label_prefix, notes_prefix,
-        scene_prefix, mark_prefix, block_prefix, follow_prefix,
-        execute_prefix):
+    annotations,
+    cue_prefix,
+    time_prefix,
+    label_prefix,
+    notes_prefix,
+    scene_prefix,
+    mark_prefix,
+    block_prefix,
+    follow_prefix,
+    execute_prefix,
+):
     # Convert a list of block-quote annotations into a cuelist table
     cue_table = [
         "| Cue Number | Time | Cue Line | Label | Notes | Scene | Mark | Block | Follow | Execute |\n",
-        "| ---------- | ---- | -------- | ----- | ----- | ----- | ---- | ----- | ------ | ------- |\n"]
+        "| ---------- | ---- | -------- | ----- | ----- | ----- | ---- | ----- | ------ | ------- |\n",
+    ]
     json_cues = []
     current_line = ["| "]
 
@@ -81,7 +117,6 @@ def process_cues(
 
     # Process the dicts and convert them to cue lines
     for i, cue in enumerate(json_cues):
-
         # Pull cue comments and convert them to useful data
         cue_comment = cue["text"]
         cue_comment = cue_comment.split("\n")
@@ -98,41 +133,50 @@ def process_cues(
         cue_execute = ""
 
         for j, comment in enumerate(cue_comment):
-            if comment.lower().startswith(cue_prefix.lower()) or \
-                    comment.startswith(cue_prefix.lower() + " "):
-                cue_number = comment[len(cue_prefix):]
+            if comment.lower().startswith(cue_prefix.lower()) or comment.startswith(
+                cue_prefix.lower() + " "
+            ):
+                cue_number = comment[len(cue_prefix) :]
 
-            if comment.lower().startswith(time_prefix.lower()) or \
-                    comment.startswith(time_prefix.lower() + " "):
-                cue_time = comment[len(time_prefix):]
+            if comment.lower().startswith(time_prefix.lower()) or comment.startswith(
+                time_prefix.lower() + " "
+            ):
+                cue_time = comment[len(time_prefix) :]
 
-            if comment.lower().startswith(label_prefix.lower()) or \
-                    comment.startswith(label_prefix.lower() + " "):
-                cue_label = comment[len(label_prefix):]
+            if comment.lower().startswith(label_prefix.lower()) or comment.startswith(
+                label_prefix.lower() + " "
+            ):
+                cue_label = comment[len(label_prefix) :]
 
-            if comment.lower().startswith(notes_prefix.lower()) or \
-                    comment.startswith(notes_prefix.lower() + " "):
-                cue_notes = comment[len(notes_prefix):]
+            if comment.lower().startswith(notes_prefix.lower()) or comment.startswith(
+                notes_prefix.lower() + " "
+            ):
+                cue_notes = comment[len(notes_prefix) :]
 
-            if comment.lower().startswith(scene_prefix.lower()) or \
-                    comment.startswith(scene_prefix.lower() + " "):
-                cue_scene = comment[len(scene_prefix):]
+            if comment.lower().startswith(scene_prefix.lower()) or comment.startswith(
+                scene_prefix.lower() + " "
+            ):
+                cue_scene = comment[len(scene_prefix) :]
 
-            if comment.lower().startswith(mark_prefix.lower()) or \
-                    comment.startswith(mark_prefix.lower() + " "):
-                cue_mark = comment[len(mark_prefix):]
+            if comment.lower().startswith(mark_prefix.lower()) or comment.startswith(
+                mark_prefix.lower() + " "
+            ):
+                cue_mark = comment[len(mark_prefix) :]
 
-            if comment.lower().startswith(block_prefix.lower()) or \
-                    comment.startswith(block_prefix.lower() + " "):
-                cue_block = comment[len(block_prefix):]
+            if comment.lower().startswith(block_prefix.lower()) or comment.startswith(
+                block_prefix.lower() + " "
+            ):
+                cue_block = comment[len(block_prefix) :]
 
-            if comment.lower().startswith(follow_prefix.lower()) or \
-                    comment.startswith(follow_prefix.lower() + " "):
-                cue_follow = comment[len(follow_prefix):]
+            if comment.lower().startswith(follow_prefix.lower()) or comment.startswith(
+                follow_prefix.lower() + " "
+            ):
+                cue_follow = comment[len(follow_prefix) :]
 
-            if comment.lower().startswith(execute_prefix.lower()) or \
-                    comment.startswith(execute_prefix.lower() + " "):
-                cue_execute = comment[len(execute_prefix):]
+            if comment.lower().startswith(execute_prefix.lower()) or comment.startswith(
+                execute_prefix.lower() + " "
+            ):
+                cue_execute = comment[len(execute_prefix) :]
 
         # Get the cue line from the annotation since it's buried in the JSON
         for j, line in enumerate(cue):
@@ -174,8 +218,8 @@ def convert_file(args):
     try:
         with open(source_file, "r") as file:
             lines = file.readlines()
-            
-    except FileDoesNotExistError:
+
+    except FileNotFoundError:
         print("Source file does not exist or path is incorrect.")
         quit()
 
@@ -230,8 +274,17 @@ def convert_file(args):
 
     # Convert the annotations into a markdown table cuelist
     md_table = process_cues(
-        annotations, cue_prefix, time_prefix, label_prefix, notes_prefix,
-        scene_prefix, mark_prefix, block_prefix, follow_prefix, execute_prefix)
+        annotations,
+        cue_prefix,
+        time_prefix,
+        label_prefix,
+        notes_prefix,
+        scene_prefix,
+        mark_prefix,
+        block_prefix,
+        follow_prefix,
+        execute_prefix,
+    )
 
     with open(dest_file, "w") as file:
         file.write(md_table)
